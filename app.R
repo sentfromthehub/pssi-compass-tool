cd47432bd9fd0ae8fb0759448328985b9b7c294a
Aadil Varachhia <121725874+sentfromthehub@users.noreply.github.com>
2026-01-09 13:56:32 -0500

    Set dark theme and update typography

--- app.R
+++ app.R
@@ -91,157 +91,150 @@ ui <- navbarPage("COMPASS",
                  header = tags$head(
                    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
                    tags$script(src = "script.js"),
+                   tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&family=Quicksand:wght@300;400;500;600;700&display=swap"),
                    useShinyjs()
                  ),
                  
                  # --- Tab 1: Home ---
                  tabPanel("Home",
-                          div(class = "dotted-page",
-                              fluidRow(
-                                column(12, align = "center",
-                                       div(class = "home-content-wrapper",
-                                           h1("COMPASS", class = "home-title"),
-                                           h4("CAMPUS ONLINE MATCHING OF PERSONALIZED ACADEMIC & STUDENT SUPPORTS", class = "home-subtitle"),
-                                           p(em("A navigational tool made \"for-students, by-students\""), class = "home-tagline"),
-                                           br(),
-                                           tags$img(src = "https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg", 
-                                                    width = "600px", class = "home-img"),
-                                           br(), br(),
-                                           actionButton("start_assessment", "START ASSESSMENT", 
-                                                        class = "btn-start"),
-                                           br(), br(),
-                                           p("POWERED BY THE LINDEN LAB", class = "footer-text")
-                                       )
-                                )
-                              )
+                          fluidRow(
+                            column(12, align = "center",
+                                   div(class = "home-content-wrapper",
+                                       h1("COMPASS", class = "home-title"),
+                                       h4("CAMPUS ONLINE MATCHING OF PERSONALIZED ACADEMIC & STUDENT SUPPORTS", class = "home-subtitle"),
+                                       p(em("A navigational tool made \"for-students, by-students\""), class = "home-tagline"),
+                                       br(),
+                                       tags$img(src = "https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg", 
+                                                width = "600px", class = "home-img"),
+                                       br(), br(),
+                                       actionButton("start_assessment", "START ASSESSMENT", 
+                                                    class = "btn-start"),
+                                       br(), br(),
+                                       p("POWERED BY THE LINDEN LAB", class = "footer-text")
+                                   )
+                            )
                           )
                  ),
                  
                  # --- Tab 2: Assessment ---
                  tabPanel("Assessment",
-                          div(class = "dotted-page",
-                              fluidRow(
-                                column(10, offset = 1, 
-                                       div(class = "content-card",
-                                           fluidRow(
-                                             column(12, align = "center",
-                                                    br(),
-                                                    h3("Student Stressor Assessment"),
-                                                    p("Select your institution and then rate your stressors in the tabs below.", class = "assess-intro"),
-                                                    div(class = "institution-select-container",
-                                                        selectInput("institution", NULL, 
-                                                                    choices = c("Concordia University", "University of Calgary", "Queen's University"), 
-                                                                    width = "100%")
-                                                    ),
-                                                    br(), br()
-                                             )
-                                           ),
-                                           fluidRow(
-                                             column(12,
-                                                    div(class = "alert alert-info", role = "alert",
-                                                        HTML("<strong>Instructions:</strong> Browse through the five domain tabs below. Rate stressors by severity on a scale of 0-10. Leave unrated (0) if not applicable.")
-                                                    )
-                                             )
-                                           ),
-                                           tabsetPanel(
-                                             id = "tabs",
-                                             tabPanel("Academic", uiOutput("academic_ui")),
-                                             tabPanel("Learning Environment", uiOutput("learning_env_ui")),
-                                             tabPanel("Campus Culture", uiOutput("campus_culture_ui")),
-                                             tabPanel("Interpersonal", uiOutput("interpersonal_ui")),
-                                             tabPanel("Personal", uiOutput("personal_ui"))
-                                           ),
-                                           br(), hr(),
-                                           fluidRow(
-                                             column(12, align = "center",
-                                                    p(em("Note: Submitting these responses does not record any data. All data is self-contained in this session and will be lost if you close the browser window."),
-                                                      class = "privacy-note"),
-                                                    actionButton("submit", "SUBMIT RESPONSES", class = "btn-primary btn-lg btn-submit-custom"),
-                                                    br()
-                                             )
-                                           )
+                          fluidRow(
+                            column(10, offset = 1, 
+                                   div(class = "content-card",
+                                       fluidRow(
+                                         column(12, align = "center",
+                                                br(),
+                                                h3("Student Stressor Assessment"),
+                                                p("Select your institution and then rate your stressors in the tabs below.", class = "assess-intro"),
+                                                div(class = "institution-select-container",
+                                                    selectInput("institution", NULL, 
+                                                                choices = c("Concordia University", "University of Calgary", "Queen's University"), 
+                                                                width = "100%")
+                                                ),
+                                                br(), br()
+                                         )
+                                       ),
+                                       fluidRow(
+                                         column(12,
+                                                div(class = "alert alert-info", role = "alert",
+                                                    HTML("<strong>Instructions:</strong> Browse through the five domain tabs below. Rate stressors by severity on a scale of 0-10. Leave unrated (0) if not applicable.")
+                                                )
+                                         )
+                                       ),
+                                       tabsetPanel(
+                                         id = "tabs",
+                                         tabPanel("Academic", uiOutput("academic_ui")),
+                                         tabPanel("Learning Environment", uiOutput("learning_env_ui")),
+                                         tabPanel("Campus Culture", uiOutput("campus_culture_ui")),
+                                         tabPanel("Interpersonal", uiOutput("interpersonal_ui")),
+                                         tabPanel("Personal", uiOutput("personal_ui"))
+                                       ),
+                                       br(), hr(),
+                                       fluidRow(
+                                         column(12, align = "center",
+                                                p(em("Note: Submitting these responses does not record any data. All data is self-contained in this session and will be lost if you close the browser window."),
+                                                  class = "privacy-note"),
+                                                actionButton("submit", "SUBMIT RESPONSES", class = "btn-primary btn-lg btn-submit-custom"),
+                                                br()
+                                         )
                                        )
-                                )
-                              )
+                                   )
+                            )
                           )
                  ),
                  
                  # --- Tab 3: Your Profile ---
                  tabPanel("Your Profile",
-                          div(class = "dotted-page",
-                              fluidRow(
-                                column(10, offset = 1,
-                                       div(class = "content-card",
-                                           fluidRow(
-                                             column(5,
-                                                    uiOutput("institution_label"),
-                                                    br(), br(),
-                                                    h4("Your Top 10 Stressors"),
-                                                    p("Based on your responses, here are the top stressors you rated highest:"),
-                                                    br(),
-                                                    uiOutput("top_stressors_table")
-                                             ),
-                                             column(7,
-                                                    h4("Your Stressors"),
-                                                    checkboxGroupInput("domain_filter", "Filter by Domain:",
-                                                                       choices = unique(pssi_items$domain),
-                                                                       selected = unique(pssi_items$domain),
-                                                                       inline = TRUE
-                                                    ),
-                                                    plotOutput("lollipop_plot", height = "600px")
-                                             )
-                                           )
+                          fluidRow(
+                            column(10, offset = 1,
+                                   div(class = "content-card",
+                                       fluidRow(
+                                         column(5,
+                                                uiOutput("institution_label"),
+                                                br(), br(),
+                                                h4("Your Top 10 Stressors"),
+                                                p("Based on your responses, here are the top stressors you rated highest:"),
+                                                br(),
+                                                uiOutput("top_stressors_table")
+                                         ),
+                                         column(7,
+                                                h4("Your Stressors"),
+                                                checkboxGroupInput("domain_filter", "Filter by Domain:",
+                                                                   choices = unique(pssi_items$domain),
+                                                                   selected = unique(pssi_items$domain),
+                                                                   inline = TRUE
+                                                ),
+                                                plotOutput("lollipop_plot", height = "600px")
+                                         )
                                        )
-                                )
-                              )
+                                   )
+                            )
                           )
                  ),
                  
                  # --- Tab 4: Recommendations ---
                  tabPanel("Recommendations",
-                          div(class = "dotted-page",
-                              fluidRow(
-                                column(10, offset = 1,
-                                       fluidRow(
-                                         # Left Sidebar Card
-                                         column(3,
-                                                div(class = "content-card",
-                                                    h4("Filters"),
-                                                    br(),
-                                                    h5("Stressors:"),
-                                                    uiOutput("stressor_filter_ui"),
-                                                    hr(),
-                                                    h5("Specific Student Groups:"),
-                                                    checkboxGroupInput("student_group_filters", label = NULL,
-                                                                       choices = c("BIPOC Students", 
-                                                                                   "International Students", 
-                                                                                   "Mature Students", 
-                                                                                   "Graduate Students", 
-                                                                                   "First Year Students", 
-                                                                                   "2SLGBTQIA+ Students", 
-                                                                                   "Indigenous Students"),
-                                                                       selected = NULL)
-                                                )
-                                         ),
-                                         # Main Results Card
-                                         column(9,
-                                                div(class = "content-card",
-                                                    fluidRow(
-                                                      column(12,
-                                                             div(class = "view-mode-container",
-                                                                 radioButtons("view_mode", "View Mode:",
-                                                                              choices = c("List View", "Map View"),
-                                                                              selected = "List View",
-                                                                              inline = TRUE)
-                                                             )
-                                                      )
-                                                    ),
-                                                    uiOutput("results_view")
-                                                )
-                                         )
-                                       )
-                                )
-                              )
+                          fluidRow(
+                            column(10, offset = 1,
+                                   fluidRow(
+                                     # Left Sidebar Card
+                                     column(3,
+                                            div(class = "content-card",
+                                                h4("Filters"),
+                                                br(),
+                                                h5("Stressors:"),
+                                                uiOutput("stressor_filter_ui"),
+                                                hr(),
+                                                h5("Specific Student Groups:"),
+                                                checkboxGroupInput("student_group_filters", label = NULL,
+                                                                   choices = c("BIPOC Students", 
+                                                                               "International Students", 
+                                                                               "Mature Students", 
+                                                                               "Graduate Students", 
+                                                                               "First Year Students", 
+                                                                               "2SLGBTQIA+ Students", 
+                                                                               "Indigenous Students"),
+                                                                   selected = NULL)
+                                            )
+                                     ),
+                                     # Main Results Card
+                                     column(9,
+                                            div(class = "content-card",
+                                                fluidRow(
+                                                  column(12,
+                                                         div(class = "view-mode-container",
+                                                             radioButtons("view_mode", "View Mode:",
+                                                                          choices = c("List View", "Map View"),
+                                                                          selected = "List View",
+                                                                          inline = TRUE)
+                                                         )
+                                                  )
+                                                ),
+                                                uiOutput("results_view")
+                                            )
+                                     )
+                                   )
+                            )
                           )
                  )
 )
